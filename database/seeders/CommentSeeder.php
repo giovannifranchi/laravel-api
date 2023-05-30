5122bdd13ffd84df14ffeea1a51226bad2fd981f
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-05-30 19:59:37 +0200

    Add: comment seeder

---
+++ database/seeders/CommentSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comment;
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CommentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 0; $i < 100; $i++){
+            $project = Project::inRandomOrder()->first();
+
+            $comment = new Comment();
+
+            $comment->author = fake()->name();
+            $comment->content = fake()->text();
+            $comment->project_id = $project->id;
+
+            $comment->save();
+        }
+    }
+}
