1b29bb003546438bc7ccf27d412f05529e48e9ad
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-05-30 18:10:14 +0200

    Add: api type controller

--- app/Http/Controllers/Api/TypeController.php
+++ app/Http/Controllers/Api/TypeController.php
@@ -3,9 +3,33 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class TypeController extends Controller
 {
-    //
+    public function index(){
+        $data = Type::with('projects')->get();
+        return response()->json($data);
+    }
+
+    public function show(string $slug){
+        $data = Type::with('projects')->where('slug', $slug)->first();
+
+        try{
+            if($data){
+                return response()->json($data);
+            }else {
+                return response()->json([
+                    'success'=>false,
+                    'results'=>null
+                ], 404);
+            }
+        }catch(\Throwable $th){
+            return response()->json([
+                'success'=>false,
+                'result'=>null
+            ], 500);
+        }
+    }
 }
