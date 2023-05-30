c203bbcb15f0b5dbc58545f446edc23833597813
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-05-30 15:26:12 +0200

    api: show added

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,7 +9,27 @@ use App\Models\Project;
 class ProjectController extends Controller
 {
     public function index(){
-        $data = Project::all();
+        $data = Project::with('type', 'technologies')->get();
         return response()->json($data);
+    }
+
+    public function show($id){
+        try{
+            $data = Project::where('id', $id)->with('type', 'technologies')->first();
+
+            if($data){
+                return response()->json($data);
+            }else {
+                return response()->json([
+                    'success'=>false,
+                    'results'=>null
+                ], 404);
+            }
+        }catch(\Throwable $th){
+            return response()->json([
+                'success' => false,
+                'results' => null
+            ], 500);
+        }
     }
 }
